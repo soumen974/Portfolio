d422e4fa8b1ec92c97133547ee8eb43ed00a93f5
Soumen Bhunia <sob99338@gmail.com>
2024-06-27 00:40:03 +0530

    versal congig and next config check

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
     images: {
       domains: ['aceternity.com'],
     },
   };
   
 export default nextConfig;
+
+
+
