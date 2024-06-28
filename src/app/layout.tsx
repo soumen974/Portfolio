edffc5b3307d1ca947971c7e09788cf8b2573646
Soumen Bhunia <sob99338@gmail.com>
2024-06-29 02:55:14 +0530

    nav top updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,9 +34,9 @@ export default function RootLayout({
       <div className="absolute -z-10 inset-x-0 -inset-y-[4rem] ">
           <BackgroundBeams/>
       </div>
-       <div className="mx-auto max-w-4xl pt-12  p-5  bg-red-00 ">
+       <div className="mx-auto max-w-4xl pt-7  p-5  bg-red-00 ">
          <div className="z-50"><Navigationbar/></div>
-         <div className="max-w-2xl mx-auto mt-28 ">
+         <div className="max-w-2xl mx-auto mt-20 ">
          {children}
          {/* <TracingBeamDemo/> */}
          </div>
