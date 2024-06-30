2ad39c122bb4439d8575615e1f0da033dfef956f
Soumen Bhunia <sob99338@gmail.com>
2024-06-30 23:07:01 +0530

    working on project section

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   const projects = [
     {
       title:"Cubikor",
-      image:<Image src="/images/cubikor.svg" className="w-fit h-60 rounded-xl " alt="SoumenBhunia" width={64} height={64} />,
+      image:<Image src="/images/cubikor.svg" className="h-full w-full  rounded-md" alt="SoumenBhunia" width={64} height={64} />,
       discription:" The Cube E-Commerce , is a web application designed to provide users with an intuitive and seamless shopping experience.and user-friendly interface for browsing and purchasing products online.",
       Skills:[
         {name:"React.js"},
@@ -17,7 +17,7 @@ export default function Home() {
     },
     {
       title:"Placement module",
-      image:<Image src="/images/cubikor.svg" className="w-fit h-60 rounded-xl " alt="SoumenBhunia" width={64} height={64} />,
+      image:<Image src="/images/cubikor.svg" className="w-fit  rounded-xl " alt="SoumenBhunia" width={64} height={64} />,
       discription:" The Cube E-Commerce , is a web application designed to provide users with an intuitive and seamless shopping experience.and user-friendly interface for browsing and purchasing products online.",
       Skills:[
         {name:"React.js"},
@@ -28,7 +28,7 @@ export default function Home() {
     },
     {
       title:"Workfolio",
-      image:<Image src="/images/cubikor.svg" className="w-fit h-60 rounded-xl " alt="SoumenBhunia" width={64} height={64} />,
+      image:<Image src="/images/workfolio.svg" className="w-fit rounded-md " alt="SoumenBhunia" width={64} height={64} />,
       discription:" The Cube E-Commerce , is a web application designed to provide users with an intuitive and seamless shopping experience.and user-friendly interface for browsing and purchasing products online.",
       Skills:[
         {name:"React.js"},
@@ -39,7 +39,7 @@ export default function Home() {
     },
     {
       title:"Cashlash",
-      image:<Image src="/images/cubikor.svg" className="w-fit h-60 rounded-xl " alt="SoumenBhunia" width={64} height={64} />,
+      image:<Image src="/images/cubikor.svg" className="w-fit  rounded-xl " alt="SoumenBhunia" width={64} height={64} />,
       discription:" The Cube E-Commerce , is a web application designed to provide users with an intuitive and seamless shopping experience.and user-friendly interface for browsing and purchasing products online.",
       Skills:[
         {name:"React.js"},
@@ -63,7 +63,7 @@ export default function Home() {
         <div className="py-8 grid grid-cols-1 sm:grid-cols-2 gap-8 md:grid-cols-2  justify-between  ">
           {projects.map((project)=>(
             <div key={project.title} className=" ">
-              <div className="border border-black/[0.2] dark:border-white/[0.2] flex flex-col items-start max-w-sm mx-auto p-4 relative ">
+              <div className="border border-black/[0.2] dark:border-white/[0.2] flex flex-col gap-4 items-start max-w-sm mx-auto p-4 relative ">
                   <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
                   <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
                   <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
@@ -72,37 +72,22 @@ export default function Home() {
                   <div className="">
                   {project.image}
                   </div>
-                  <div className="">
-                    <h1>{project.title}</h1>
-                  </div>
 
-                  <h2 className="dark:text-white text-black mt-4 text-sm font-light">
-                    {project.discription}
-                  </h2>
-            
+                  <div className="grid gap-2">
+                      <div className="">
+                        <h1>{project.title}</h1>
+                      </div>
+
+                      <h2 className="dark:text-white text-black  text-sm font-light">
+                        {project.discription}
+                      </h2>
+                    </div>
               </div>
             
             </div>
 
           ))}
         </div>
-
-        <div className="pt-5 grid md:flex-col-2  gap-5 md:flex">
-            {projects.map((project)=>(
-              <div key={project.title} className="border-[#ababb25a] grid gap-y-3 border-[1.6px] w-fit px-6 py-8 rounded-xl sm:rounded-md">
-                  <h1 className='font-bold text-xl'>{project.title}</h1>
-                  <p className='font-thin'>
-                    {project.discription}
-                  </p>
-                  <div className=" flex flex-wrap gap-2 w-[90%]">
-                    {project.Skills.map((skill)=>(
-                      <h1 key={skill.name} className='odd:bg-black  even:bg-indigo-400 w-fit rounded-full text-sm t px-[0.6rem]'>{skill.name}</h1>
-                    ))}
-                  </div>
-              </div>
-            ))}
-          </div>
-          
       </div>
     </div>
    </>
